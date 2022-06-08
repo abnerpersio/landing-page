f79c8e754ff8a36daeb0c39cdaa91aad323c40fc
Abner Persio <binhopersio@gmail.com>
2022-06-07 22:30:56 -0300

    feat: add styled components

---
+++ src/components/App/index.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+
+import { GlobalStyles } from '../../styles/global';
+import { defaultTheme } from '../../styles/themes/default';
+import { Router } from '../Router';
+
+export function App() {
+  return (
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyles />
+
+        <Router />
+      </ThemeProvider>
+    </BrowserRouter>
+  );
+}
---
+++ src/styles/global.ts
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    outline: none;
+    font-family: 'Inter', 'Lato', sans-serif;
+    box-sizing: border-box;
+  }
+  
+  body, html {
+    width: 100%;
+    height: 100%;
+  }
+`;
